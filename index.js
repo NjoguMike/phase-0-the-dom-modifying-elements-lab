2bd77873d9ad64cbcc79ba3b432774f3f2228223
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-07-06 14:36:14 +0300

    Completed DOM

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const removeMain = document.querySelector('#main')
+removeMain.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.className = 'H1';
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Mwagash is the champion'
+document.body.append(newHeader)
+
+
+
+
