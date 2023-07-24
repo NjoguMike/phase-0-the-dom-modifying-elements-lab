3257b3e74b1c34e9148e187fb66dde423c39d9fe
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-07-24 23:29:41 +0300

    Revisited Phase1 complete

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 // Write your code here!
-const removeMain = document.querySelector('#main')
-removeMain.remove()
 
-const newHeader = document.createElement('h1');
-newHeader.className = 'H1';
-newHeader.id = 'victory';
-newHeader.innerHTML = 'Mwagash is the champion'
-document.body.append(newHeader)
+const main = document.getElementById('main')
+main.remove()
 
-
-
-
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Michael is the champion'
